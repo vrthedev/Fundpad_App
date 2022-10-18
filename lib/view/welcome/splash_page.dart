64822574fbbacac004e79863c68a3a4025773b9c
Mardel <mardeli2120@gmail.com>
2022-10-18 15:41:41 +0200

    icon size

--- lib/view/welcome/splash_page.dart
+++ lib/view/welcome/splash_page.dart
@@ -37,7 +37,7 @@ class _SplashPageState extends State<SplashPage> {
           const SizedBox(height: 40),
           Image.asset(
             'images/logol1.png',
-            height: MediaQuery.of(context).size.width / 2,
+            scale: 9,
           ),
         ]),
       ),
